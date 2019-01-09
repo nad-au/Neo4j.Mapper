844cafe50fb882841113bbd0f1d97748bda72858
Neil Dobson <ndobson@barnardos.org.au>
2019-01-09 21:34:22 +1100

    Refactor NodeIdPropertyAccessor. Add UpdateNode extension

--- src/Neo4jMapper/SessionExtensions.cs
+++ src/Neo4jMapper/SessionExtensions.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Linq.Expressions;
 using System.Reflection;
 using Neo4j.Driver.V1;
@@ -27,6 +28,22 @@ namespace Neo4jMapper
             };
 
             return session.Run(statement, parameters);
+        }
+
+        public static IStatementResult UpdateNode<T>(
+            this ISession session,
+            T entity) where T : class
+        {
+            const string statement = @"
+                MATCH (node)
+                WHERE id(node) = $p1
+                SET node = $p2";
+
+            var nodeId = entity.GetNodeId();
+            if (nodeId == null)
+                throw new Exception("NodeIdAttribute not specified or the Node Id is null");
+
+            return session.Run(statement, nodeId.Value);
         }
     }
 }

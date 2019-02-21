ad8af6939935932e71590230f3fae76a5329a9b3
Neil Dobson <ndobson@barnardos.org.au>
2019-02-21 14:28:37 +1100

    Changes to ServiceStack APIs.

--- src/Neo4jMapper/Neo4jMapperConfig.cs
+++ src/Neo4jMapper/Neo4jMapperConfig.cs
@@ -17,26 +17,26 @@ namespace Neo4jMapper
 
         public static void RegisterLocalDateToDateTimeConverter()
         {
-            AutoMappingUtils.RegisterConverter((LocalDate localDate) => localDate.ToDateTime());
-            AutoMappingUtils.RegisterConverter((DateTime dateTime) => new LocalDate(dateTime));
+            AutoMapping.RegisterConverter((LocalDate localDate) => localDate.ToDateTime());
+            AutoMapping.RegisterConverter((DateTime dateTime) => new LocalDate(dateTime));
         }
 
         public static void RegisterLocalTimeToTimeSpanConverter()
         {
-            AutoMappingUtils.RegisterConverter((LocalTime localTime) => localTime.ToTimeSpan());
-            AutoMappingUtils.RegisterConverter((TimeSpan timeSpan) => new LocalTime(timeSpan));
+            AutoMapping.RegisterConverter((LocalTime localTime) => localTime.ToTimeSpan());
+            AutoMapping.RegisterConverter((TimeSpan timeSpan) => new LocalTime(timeSpan));
         }
 
         public static void RegisterZonedDateTimeToDateTimeOffsetConverter()
         {
-            AutoMappingUtils.RegisterConverter((ZonedDateTime zonedDateTime) => zonedDateTime.ToDateTimeOffset());
-            AutoMappingUtils.RegisterConverter((DateTimeOffset dateTimeOffset) => new ZonedDateTime(dateTimeOffset));
+            AutoMapping.RegisterConverter((ZonedDateTime zonedDateTime) => zonedDateTime.ToDateTimeOffset());
+            AutoMapping.RegisterConverter((DateTimeOffset dateTimeOffset) => new ZonedDateTime(dateTimeOffset));
         }
 
         public static void RegisterLocalDateTimeToDateTimeConverter()
         {
-            AutoMappingUtils.RegisterConverter((LocalDateTime localDateTime) => localDateTime.ToDateTime());
-            AutoMappingUtils.RegisterConverter((DateTime dateTime) => new LocalDateTime(dateTime));
+            AutoMapping.RegisterConverter((LocalDateTime localDateTime) => localDateTime.ToDateTime());
+            AutoMapping.RegisterConverter((DateTime dateTime) => new LocalDateTime(dateTime));
         }
     }
 }

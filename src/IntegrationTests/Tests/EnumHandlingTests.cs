4d223fd69bef2f1f52c9677df2cc387f7d0a178d
bnaya eshet <bnayanet@hotmail.com>
2020-01-23 08:35:19 +0200

    pass tests

--- src/IntegrationTests/Tests/EnumHandlingTests.cs
+++ src/IntegrationTests/Tests/EnumHandlingTests.cs
@@ -1,9 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics.CodeAnalysis;
-using System.Linq;
 using System.Threading.Tasks;
-using IntegrationTests.Models;
+using Neo4j.Driver.V1;
 using Neo4jMapper;
 using NUnit.Framework;
 
@@ -11,47 +10,52 @@ namespace IntegrationTests.Tests
 {
     [TestFixture]
     [NonParallelizable]
-    public class EnumHandlingTests : MoviesFixtureBase
+    public class EnumHandlingTests : TestFixtureBase
     {
         [Test]
         public async Task Should_Map_Enum()
         {
-            await Session.RunAsync(@"
+            using (var session = Driver.Session(AccessMode.Write))
+            {
+                await session.RunAsync(@"
                 MATCH (n:Payload {Id: 1})
                 Detach Delete n").ConfigureAwait(false);
 
-            var payload = new Payload
-            {
-                Id = 1,
-                Color = ConsoleColor.Red,
-                Name = "Test 1"
-            };
+                var payload = new Payload
+                {
+                    Id = 1,
+                    Color = ConsoleColor.Red,
+                    Name = "Test 1"
+                };
 
-            var parms = new Neo4jParameters()
-             .WithEntity<Payload>("map", payload,
-                        (k, v) => k == nameof(Payload.Color) ? v.ToString() : v);
+                var parms = new Neo4jParameters()
+                 .WithEntity<Payload>("map", payload,
+                            (k, v) => k == nameof(Payload.Color) ? v.ToString() : v);
 
-            var cursor = await Session.RunAsync(@"
+                var cursor = await session.RunAsync(@"
                 CREATE (n:Payload $map)
                 RETURN n", parms).ConfigureAwait(false);
 
-            var result = await cursor
-                .MapSingleAsync<Payload>().ConfigureAwait(false);
+                var result = await cursor
+                    .MapSingleAsync<Payload>().ConfigureAwait(false);
 
-            Assert.AreEqual(payload, result);
+                Assert.AreEqual(payload, result);
+            }
         }
         [Test]
         public async Task Should_Map_Entities_Enum()
         {
-            await Session.RunAsync(@"
+            using (var session = Driver.Session(AccessMode.Write))
+            {
+                await session.RunAsync(@"
                 MATCH (n:Payload {Id: 1})
                 Detach Delete n").ConfigureAwait(false);
-            await Session.RunAsync(@"
+                await session.RunAsync(@"
                 MATCH (n:Payload {Id: 2})
                 Detach Delete n").ConfigureAwait(false);
 
-            var payloads = new[]
-            {
+                var payloads = new[]
+                {
                 new Payload
                 {
                     Id = 1,
@@ -66,18 +70,21 @@ namespace IntegrationTests.Tests
                 },
             };
 
-            var parms = new Neo4jParameters()
-             .WithEntities<Payload>("map", payloads,
-                        (k, v) => k == nameof(Payload.Color) ? v.ToString() : v);
+                var parms = new Neo4jParameters()
+                 .WithEntities<Payload>("items", payloads,
+                            (k, v) => k == nameof(Payload.Color) ? v.ToString() : v);
 
-            var cursor = await Session.RunAsync(@"
-                CREATE (n:Payload $map)
+                var cursor = await session.RunAsync(@"
+                UNWIND $items AS item
+                CREATE (n:Payload)
+                SET n = item 
                 RETURN n", parms).ConfigureAwait(false);
 
-            var result = await cursor
-                .MapAsync<Payload>().ConfigureAwait(false);
+                var result = await cursor
+                    .MapAsync<Payload>().ConfigureAwait(false);
 
-            Assert.AreEqual(payloads, result);
+                Assert.AreEqual(payloads, result);
+            }
         }
         class Payload : IEquatable<Payload>
         {

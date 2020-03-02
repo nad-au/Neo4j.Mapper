b8b5bc5687fbefdd50ff613176db7425034bb18e
Neil Dobson <neil.dobson71@gmail.com>
2020-03-02 14:38:53 +1100

    Move one-time set-up to separate class

--- src/IntegrationTests/TestFixtureBase.cs
+++ src/IntegrationTests/TestFixtureBase.cs
@@ -11,21 +11,11 @@ namespace IntegrationTests
         protected virtual void TestFixtureSetUp()
         {
             Driver = GraphDatabase.Driver("bolt://localhost:7687");
-
-            // Ensure node exists to avoid flaky test where node id = 0
-            using (var session = Driver.Session())
-            {
-                session.Run("MERGE (:Neo4jMapperTest)");
-            }
         }
 
         [OneTimeTearDown]
         protected virtual void TestFixtureTearDown()
         {
-            using (var session = Driver.Session())
-            {
-                session.Run("MATCH (n:Neo4jMapperTest) DELETE n");
-            }
             Driver.Dispose();
         }
     }

7dabb6cda109dba5470cc7d45b067195480c4f5a
Neil Dobson <neil.dobson71@gmail.com>
2022-02-06 19:36:30 +1100

    Cleanup

--- src/Publish.ps1
+++ src/Publish.ps1
@@ -1,4 +1,2 @@
 dotnet pack ./Neo4jMapper/Neo4jMapper.csproj -c Release
 dotnet pack ./Neo4jMapper.Signed/Neo4jMapper.Signed.csproj -c Release
-dotnet pack ./Neo4jMapper.Core/Neo4jMapper.Core.csproj -c Release
-dotnet pack ./Neo4jMapper.Core.Signed/Neo4jMapper.Core.Signed.csproj -c Release
